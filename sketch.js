f01ddb3da26818f932b947338729f66ff99fec9d
Tuyet Le <tle83@lsu.edu>
2017-02-23 10:44:25 -0600

    Buttons Added
    =Buttons with changing text color to indicate what is being pressed

--- sketch.js
+++ sketch.js
@@ -1,33 +1,20 @@
 var title = "Basic Sound Synthesis"
 //var synth, synth2;
 var osc, ampEnv, osc2;
+var c1, c2, c3, c4, c5;
+var fil;
 
 function setup(){
 	createCanvas(640, 400);
-/*
-	synth = new Tone.Synth({
-		oscillator:{
-			type:"triangle"
-		},
-		envelope:{
-			attack:0.005,
-			decay:0.1,
-			sustain:0.3,
-			release:1
-		}
-	}).toMaster();
-
-	synth.envelope.attack = 2;
-
-	synth2 = new Tone.Synth().toMaster();
-
-	synth.triggerAttackRelease('C4', 3);
-	synth2.triggerAttackRelease('G4', '8n', +1, 0.5);
-*/
+	c1 = color(0);
+	c2 = color(0);
+	c3 = color(0);
+	c4 = color(0);
+	c5 = color(0);
 
 	osc = new Tone.Oscillator(440, "sine").start();
 	osc2 = new Tone.Oscillator(660, "sawtooth6").start();
-	osc2.volume.value = -12;
+	osc2.volume.value = -5;
 
 	ampEnv = new Tone.AmplitudeEnvelope({
 		"attack": 0.1,
@@ -39,62 +26,129 @@ function setup(){
 	osc.connect(ampEnv);
 	osc2.connect(ampEnv);
 
-//	ampEnv.triggerAttackRelease(1);
-//	osc.frequency.value = "C6";
+	fil = new Tone.Filter({
+		type:"lowpass",
+		frequency:350,
+		rolloff:-12,
+		Q:1,
+		gain:0
+	});
+
+	osc.connect(fil);
+	osc2.connect(fil);
+
 }
 
 function draw(){
-	fill(100);
-	rect(0, 0, width, height);
+	background(100);
+	/*
+	for(var i = 0; i <= 10; i++){
+		stroke(255);
+		fill(random(100, 255), random(100, 255), random(100,255));
+		ellipse(random(640), random(400), 50, 50);
+	}
+	*/
+	noStroke();
 	fill(255);
-	textAlign(10, 10);
 	text(title, 600/2.5, 50);
 
+	stroke(255);
+	strokeWeight(2);
+	//W
 	fill(225, 255, 191);
 	rect(60, 100, 250, 100);
+	//D
 	fill(183, 251, 215);
-	rect(60 + 250, 100, 250, 100);
+	rect(60 + 260, 90, 250, 100);
+	//A
 	fill(157, 236, 238);
-	rect(60, 100 + 100, 250, 100);
+	rect(50, 100 + 110, 250, 100);
+	//S
 	fill(147, 191, 231);
 	rect(60 + 250, 100 + 100, 250, 100);
+	//F
 	fill(123, 147, 231);
 	rect(60 + 130, 100 + 50, 250, 100);
 
-	fill(0);
-	stroke(255);
+	noStroke();
 	textSize(20);
+	fill(c1);
 	text("Press W", 100, 150);
-	text("Press A", 100, 250);
-	text("Press S", 450, 250);
+	fill(c2);
+	text("Press A", 100, 260);
+	fill(c3);
+	text("Press S", 450, 260);
+	fill(c4);
 	text("Press D", 450, 150);
-	text("Press F", 280, 200);
+	fill(c5);
+	text("Press F", 280, 210);
 
 }
+
 
 function keyPressed(){
 	console.log("Key is: ", keyCode);
 	//press W
 	if (keyCode == 87){
+		osc.frequency.value = "F5";
+		osc2.frequency.value = "C5";
+		ampEnv.triggerAttackRelease(1);
+		osc.frequency.setValueAtTime("A3", +0.5);
 
+		fil.type = "lowshelf";
+
+		c2 = color(0);
+		c3 = color(0);
+		c4 = color(0);
+		c5 = color(0);
+		c1 = color(183, 87, 158);
 	}
 	//press A
 	else if(keyCode == 65){
 		osc.frequency.value = 'C5';
-		ampEnv.triggerAttackRelease(1.0);
+		ampEnv.triggerAttackRelease(1);
 		osc.frequency.setValueAtTime('D5', +0.5);
+
+		c1 = color(0);
+		c3 = color(0);
+		c4 = color(0);
+		c5 = color(0);
+		c2 = color(183, 87, 158);
 	}
 	//press S
 	else if(keyCode == 83){
 		osc.frequency.value = 'Bb4';
-		ampEnv.triggerAttackRelease(0.5);
+		ampEnv.triggerAttackRelease(1);
+		osc.frequency.setValueAtTime("G5", +0.5);
+
+		c1 = color(0);
+		c2 = color(0);
+		c4 = color(0);
+		c5 = color(0);
+		c3 = color(183, 87, 158);
 	}
 	//press D
 	else if(keyCode == 68){
+		osc.frequency.value = 'B6';
+		ampEnv.triggerAttackRelease(1);
+		osc.frequency.setValueAtTime("A5", +0.5);
 
+		c1 = color(0);
+		c2 = color(0);
+		c3 = color(0);
+		c5 = color(0);
+		c4 = color(183, 87, 158);
 	}
 	//press F
 	else if(keyCode ==  70){
-		
+		osc.frequency.value = 'C4';
+		ampEnv.triggerAttackRelease(1);
+		osc.frequency.setValueAtTime("B5", +0.5);
+
+		c1 = color(0);
+		c2 = color(0);
+		c3 = color(0);
+		c4 = color(0);
+		c5 = color(183, 87, 158);
 	}
 }
